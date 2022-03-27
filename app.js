ec6ae0a32fcc66e572926f751aea47247d0b5298
Auddity <audsecondarylap@outlook.com>
2022-03-27 14:09:27 -0400

    mobile menu toggle transition

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 import get from "./getElement.js";
 
-const dropDownMenu = get('drop-down-menu')
+// Mobile Drop Down Menu
+const dropDownLinks = get('drop-down-links');
+const dropDownContainer = get('drop-down-container');
 get('menu-btn').addEventListener('click', () => {
-  dropDownMenu.classList.toggle('show');
+  const linksHeight = dropDownLinks.getBoundingClientRect().height;
+  const containerHeight = dropDownContainer.getBoundingClientRect().height;
+  containerHeight === 0 
+    ? dropDownContainer.style.height = `${linksHeight}px`
+    : dropDownContainer.style.height = 0;
 });
